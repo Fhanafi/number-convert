fec780997073d74281b3dfe98e269e5231b00503
Fhanafi <faisalhanafi2806@gmail.com>
2022-10-11 08:50:45 +0700

    num to bin

---
+++ numtobin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  int b[10],n,i;
+  cout<<"Masukan Angka : ";
+  cin>>n;
+  for(i=0; n>0; i++){
+    b[i]=n%2;
+    n=n/2;
+  }
+  cout<<" Hasil Binnarya : ";
+  for(i=i-1; i>=0; i--) {
+    cout<<b[i];
+  }
+}
